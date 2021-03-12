3cfae5e1fd765672c3bb855d337758d91817f9c7
revuttskaya <revutskaya.ke@gmail.com>
2021-03-13 00:23:41 +0300

    init: start reverse-int task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    let arr = n.toString().split('');
+    let reverseArr = [];
+    for (let i = (arr.length - 1); i >= 0; i--) {
+        reverseArr.push(arr[i]);
+    }
+    return Number.parseInt(reverseArr.join(''));
 }
